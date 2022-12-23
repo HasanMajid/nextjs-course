b4f472f0e74e61821e8791b249b8ef08037ef594
Hasan Majid <hasanmajid3.14@hotmail.com>
2022-12-23 17:52:06 -0500

    navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,20 @@ import { useRouter } from "next/router";
 function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query); // { id: 'max' }
+
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/max/project1"); // alternative way to do this
+    router.push({
+      pathname: "/clients/[id]/[clientproject]",
+      query: { id: "max", clientproject: "project1" },
+    });
+    // router.replace("/clients/max/project1"); // replace the current page with this page (you can't go back to this previous page)
+  }
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
