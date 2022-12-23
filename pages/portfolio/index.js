5917e6bc1805d9148fedf626cd73edd9d1c44a87
Hasan Majid <hasanmajid3.14@hotmail.com>
2022-12-23 16:00:22 -0500

    added nested paths and routes

---
+++ pages/portfolio/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PortfolioPage() {
+  return (
+    <h1>Portfolio Page</h1>
+  )
+}
---
+++ pages/portfolio/list.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ListPage() {
+  return (
+    <>
+      <h1>Portfolio List Page!</h1>
+      <p>My awesome portfolio list</p>
+    </>
+  );
+}
+
+export default ListPage;
