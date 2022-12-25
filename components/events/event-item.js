6302e8f8961f7b75548fd85800197fe1ff4d082e
Hasan Majid <hasanmajid3.14@hotmail.com>
2022-12-25 10:05:26 -0500

    added button icons and style for them

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
 import Link from "next/link";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 import classes from "./event-item.module.css";
 
 function EventItem(props) {
@@ -19,14 +23,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedLocation}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
---
+++ components/ui/button.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import classes from "./button.module.css";
+
+function Button(props) {
+  return (
+    <Link href={props.link} className={classes.btn}>
+      {props.children}
+    </Link>
+  );
+}
+
+export default Button;
