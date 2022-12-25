3589d84a22065e1d955c4bf19205fd651d6b341b
Hasan Majid <hasanmajid3.14@hotmail.com>
2022-12-24 21:49:38 -0500

    add css module styles for components

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,30 +1,33 @@
 import Link from "next/link";
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US',{
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    }) // convert date to human readable date
-    const formattedLocation = location.replace(', ', '\n') // replace comma with new line
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }); // convert date to human readable date
+  const formattedLocation = location.replace(", ", "\n"); // replace comma with new line
 
-    const exploreLink = `/events/${id}`
+  const exploreLink = `/events/${id}`;
 
   return (
-    <li>
-      <img src={'/' + image} alt="" /> 
-      <div>
-        <h2>{title}</h2>
-        <div>
-          <time>{humanReadableDate}</time>
+    <li className={classes.item}>
+      <img src={"/" + image} alt="" />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedLocation}</address>
+          </div>
         </div>
-        <div>
-          <address>{formattedLocation}</address>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
         </div>
-      </div>
-      <div>
-        <Link href={exploreLink}>Explore Event</Link>
       </div>
     </li>
   );
