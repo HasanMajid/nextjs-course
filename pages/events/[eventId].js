f1fe35590bc23d7398498113af150e779d898ac5
Hasan Majid <hasanmajid3.14@hotmail.com>
2022-12-31 06:36:43 -0500

    added event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,12 +1,35 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React, { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail//event-logistics";
+import EventContent from "../../components/event-detail//event-content";
 
 function EventDetailPage() {
-    const router = useRouter()
-    const eventId = router.query.eventId
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <h1>Event Number {eventId}</h1>
-  )
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
-export default EventDetailPage
+export default EventDetailPage;
