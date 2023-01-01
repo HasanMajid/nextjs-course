0a1e936260c3c2d04daedbe5c4c1433943aecacc
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-01-01 02:52:49 -0500

    removed ul tags

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import {getFeaturedEvents} from '../dummy-data';
-import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
-      <ul>
-        <EventList items={featuredEvents} />
-      </ul>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
