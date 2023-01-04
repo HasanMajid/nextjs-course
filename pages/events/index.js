a7286fd490664c235c9c0e0129d9dcc0b27a54d1
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-01-04 11:37:26 -0500

    added filter form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { getAllEvents } from '../../dummy-data'
-import EventList from '../../components/events/event-list'
+import React, { Fragment } from "react";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
-  )
+    </Fragment>
+  );
 }
 
-export default AllEventsPage
+export default AllEventsPage;
